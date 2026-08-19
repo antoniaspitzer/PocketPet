f0c530d21a7776ea72bffa5647765068cfc6d1d5
antoniaspitzer <s2410238084@fhooe.at>
2026-08-19 11:21:07 +0200

    made the first version of the buttons

--- src/main/java/com/pocketpet/ui/PixelButton.java
+++ src/main/java/com/pocketpet/ui/PixelButton.java
@@ -1,13 +1,59 @@
 package com.pocketpet.ui;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 
 public class PixelButton extends Button {
+
+    private final Image standardButton = new Image(
+        getClass()
+            .getResource("/images/functional/button-big-standart.png")
+            .toExternalForm()
+    );
+
+    private final Image pressedButton = new Image(
+        getClass()
+            .getResource("/images/functional/button-big-onclick.png")
+            .toExternalForm()
+    );
 
     public PixelButton(String text) {
         super(text);
 
         setPrefWidth(100);
         setPrefHeight(40);
+
+        // Normaler Button
+        setStyle(
+            "-fx-background-image: url('" + standardButton.getUrl() + "');" +
+            "-fx-background-size: 100% 100%;" +
+            "-fx-background-repeat: no-repeat;" +
+            "-fx-background-color: transparent;" +
+            "-fx-text-fill: white;"
+        );
+
+        // Wenn der Button gedrückt wird
+        setOnMousePressed(event -> {
+
+            setStyle(
+                "-fx-background-image: url('" + pressedButton.getUrl() + "');" +
+                "-fx-background-size: 100% 100%;" +
+                "-fx-background-repeat: no-repeat;" +
+                "-fx-background-color: transparent;" +
+                "-fx-text-fill: white;"
+            );
+        });
+
+        // Wenn man loslässt
+        setOnMouseReleased(event -> {
+
+            setStyle(
+                "-fx-background-image: url('" + standardButton.getUrl() + "');" +
+                "-fx-background-size: 100% 100%;" +
+                "-fx-background-repeat: no-repeat;" +
+                "-fx-background-color: transparent;" +
+                "-fx-text-fill: white;"
+            );
+        });
     }
 }
