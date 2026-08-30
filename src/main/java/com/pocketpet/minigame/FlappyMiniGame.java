538f370ba4975080cbf43ccdf1c2c7ccb7328f01
Antonia Spitzer <s2410238084@fhooe.at>
2026-08-30 08:37:09 +0200

    FlappyMiniGame.java erstellen

---
+++ src/main/java/com/pocketpet/minigame/FlappyMiniGame.java
@@ -0,0 +1,15 @@
+// here will be the code for a flappy bird 
+// game
+
+
+// first we have to see what color your pet
+// has and which type of pet it is!
+// then we will use an image sequence. 
+// everytime you tap/you press the space bar
+// it will flap its arms down and back up
+
+// we have to get kind of a small physics engine 
+// bzw the pet/image should fall always 
+// the same (using a graph?)
+
+// there will be randomized 
