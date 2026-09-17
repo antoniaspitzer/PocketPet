bd997ff1b881b334e02449088fc175f740108caf
Antonia Spitzer <s2410238084@fhooe.at>
2026-09-17 23:28:48 +0200

    ShopScreen.java aktualisieren

--- src/main/java/com/pocketpet/ui/ShopScreen.java
+++ src/main/java/com/pocketpet/ui/ShopScreen.java
@@ -1,3 +1,8 @@
 // hier soll der shop rein!
 // es soll wieder der selbe frame geladen 
 // werden wie bei den anderen screens
+
+// oben soll eine leiste mit verschiedenen
+// kategorien geben. also zB ("food", "hats",
+// "minigames") 
+
