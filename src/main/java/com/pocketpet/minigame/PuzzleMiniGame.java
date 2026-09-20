fc435f95bad51c4fdb38fa31f1bbcef14e93bd4e
Antonia Spitzer <s2410238084@fhooe.at>
2026-09-20 20:58:06 +0200

    PuzzleMiniGame.java aktualisieren

--- src/main/java/com/pocketpet/minigame/PuzzleMiniGame.java
+++ src/main/java/com/pocketpet/minigame/PuzzleMiniGame.java
@@ -1,2 +1,7 @@
 // this should end up being a 
 // schiebepuzzle
+// so there will be a 4x4 grid. one should 
+// be left out the rest should be able
+// to move! there will be numbers at furst
+// but lateronbi want to have a pic
+// of your pet 
